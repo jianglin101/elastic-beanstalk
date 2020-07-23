070c28f8510a40f9a058e34bbc9c5e1972b06dae
jianglin101 <jianglin101@gmail.com>
2020-07-23 16:49:07 -0400

    Update app.js
    remove db connection

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require('express')
 const http = require('http')
 
 const app = express()
-const connectToDatabase = require('./db')
+// const connectToDatabase = require('./db')
 const Note = require('./Note')
 
 app.get('/', async (req, res) => {
-    await connectToDatabase()
-    const notes = await Note.find()
-    res.send({ v: 1, notes })
+    // await connectToDatabase()
+    // const notes = await Note.find()
+    res.send({ v: 1, message: 'success' })
 })
 
 http.createServer(app).listen(process.env.PORT || 8000)
